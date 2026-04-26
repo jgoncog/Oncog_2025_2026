109b27cd362008af93fb2e6eb646768a5c847408
jgoncog <jgoncog@cvisc.pshs.edu.ph>
2026-04-26 20:17:52 +0800

    dictionary 1

---
+++ OncogJosueRuch_CS2_Q3_Act1.py
@@ -0,0 +1,20 @@
+# OncogJosueRuch_CS2_Q3_Act1.py
+# Activity Set 1: Student Profile Creator (Using Dictionary)
+
+# Step 1: Create an empty dictionary
+student = {}
+
+# Step 2: Collect input() from the user
+
+student["name"] = input("Enter your name: ")
+student["age"] = input("Enter your age: ")
+student["subject"] = input("Enter your favorite subject: ")
+
+# Step 3: Print the student's information clearly
+print("\nStudent Record:")
+print("Name:", student.get("name"))       
+print("Age:", student.get("age"))
+print("Favorite Subject:", student.get("subject"))
+
+
+#Happy New Year Sir ( Sorry About Danielle =[   )
